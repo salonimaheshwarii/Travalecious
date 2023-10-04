a96fe719aa24a564966918361395f2bcaa0dffc6
Bhavin Bhatnagar <mac@Macs-MacBook-Pro.local>
2023-10-05 02:02:04 +0530

    home-page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import 'tailwindcss/tailwind.css'; // This assumes you're using Just-in-Time (JIT) mode
+
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      {/* <Login/> */}
+      <HomePage/>
+      
     </div>
   );
 }
